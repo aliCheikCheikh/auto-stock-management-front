8bad3052502b60e0a84bb7e81a0608e3fe4a1bc0
Ali Cheikh <ali.cheikh@etu.univ-nantes.fr>
2026-05-26 18:09:33 +0200

    feat(products): rename baseUrl to apiBaseUrl

--- src/app/features/products/data-access/products-api.service.ts
+++ src/app/features/products/data-access/products-api.service.ts
@@ -1,18 +1,19 @@
-import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Page } from "../../../core/api/page.model";
+import { Product } from "../models/product.model";
 
-import { Page } from '../../../core/api/page.model';
-import { Product } from '../models/product.model';
+
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ProductsApiService {
-  private readonly http = inject(HttpClient);
-  private readonly apiBaseUrl = '/api/v1';
+    private readonly http = inject(HttpClient);
+    private readonly apiBaseUrl = '/api/v1';
 
-  listProducts(): Observable<Page<Product>> {
-    return this.http.get<Page<Product>>(`${this.apiBaseUrl}/products`);
-  }
+    public listProducts(): Observable<Page<Product>> {
+        return this.http.get<Page<Product>>(`${this.apiBaseUrl}/products`);
+    }
 }
