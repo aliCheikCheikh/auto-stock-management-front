abd62352a28a564af1c912645ebce267c757b86d
Ali Cheikh <ali.cheikh@etu.univ-nantes.fr>
2026-05-26 13:15:11 +0200

    feat(products): add products page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ProductsPage } from './features/products/pages/products-page/products-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'products',
+        component: ProductsPage
+    },
+
+    {
+        path: '',
+        redirectTo: 'products',
+        pathMatch: 'full'
+    }
+];
