1fd40b19a67e9e729a1aab0db90ee818e28b7894
Ali Cheikh <ali.cheikh@etu.univ-nantes.fr>
2026-05-26 18:25:35 +0200

    feat(stock-receipts): add receipt form shell

--- src/app/features/stock-receipts/pages/new-stock-receipt-page/new-stock-receipt-page.ts
+++ src/app/features/stock-receipts/pages/new-stock-receipt-page/new-stock-receipt-page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-stock-receipt-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-stock-receipt-page.html',
   styleUrl: './new-stock-receipt-page.scss',
 })
 export class NewStockReceiptPage {
-
+  readonly form = new FormGroup({
+    productReference: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+  });
 }
