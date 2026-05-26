4ca91622d4c749e327e6e9d87441202e6149b808
Ali Cheikh <ali.cheikh@etu.univ-nantes.fr>
2026-05-26 18:07:53 +0200

    feat(stock-receipts): add new receipt page route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { ProductsPage } from './features/products/pages/products-page/products-page';
+import { NewStockReceiptPage } from './features/stock-receipts/pages/new-stock-receipt-page/new-stock-receipt-page';
 
 export const routes: Routes = [
     {
         path: 'products',
         component: ProductsPage
+    },
+
+    {
+        path: 'stock-receipts/new',
+        component: NewStockReceiptPage
+
     },
 
     {
---
+++ src/app/features/stock-receipts/pages/new-stock-receipt-page/new-stock-receipt-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-stock-receipt-page',
+  imports: [],
+  templateUrl: './new-stock-receipt-page.html',
+  styleUrl: './new-stock-receipt-page.scss',
+})
+export class NewStockReceiptPage {
+
+}
